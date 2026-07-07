ebcfca8fba5a8b5cf7e1533c30284a52f903f85d
Irana1 <79069530+Irana1@users.noreply.github.com>
2026-07-07 16:23:44 -0400

    Add files via upload

--- documentation.md
+++ documentation.md
@@ -2,3 +2,6 @@
 
 Had the first issue with getting the task input value showing up in the created task div in the submit event. First problem I found was **the button was not wrapped in a form** All buttons that have a submit type has to be wrapped in a form. The other problem I found was **the form ended early**. The form had a /form early in the form that made the form end early.
 
+## Second Issue
+
+Second issue was that the div wou'dnt display the task text after creating the task object. **Simple fix is to create a task span, set it's text content to the tasks' text value, and append the span to the task div.**
--- script.js
+++ script.js
@@ -3,18 +3,52 @@ document.addEventListener("DOMContentLoaded", function() {
     const taskInput = document.querySelector("#task-input");
     const taskContainer = document.querySelector("#task-container");
 
+    let taskList = []
+    let currentId = 1
+
     taskForm.addEventListener("submit", function(event) {
         event.preventDefault();
 
         const taskText = taskInput.value.trim();
 
         if (taskText === "") {
-            console.log("Input cannot be empty");
+            window.alert("Input cannot be empty.")
+            taskInput.value = ""
         } else {
+            const task = {
+                id: currentId++,
+                text: taskText,
+                completed: false
+            }
+
+            taskList.push(task);
+
             const taskDiv = document.createElement("div");
-            taskDiv.textContent = taskText;
+
+            const taskSpan = document.createElement("span");
+            taskSpan.textContent = task.text;
+
+            const editButton = document.createElement("button");
+            editButton.textContent = "Edit";
+            editButton.addEventListener("click", function() {
+                task.text = window.prompt("What would you like to edit the task to?");
+                taskSpan.textContent = task.text;
+            })
+
+            const deleteButton = document.createElement("button");
+            deleteButton.textContent = "Delete";
+            deleteButton.addEventListener("click", () => {
+                taskDiv.remove();
+            })
+
+            taskDiv.appendChild(taskSpan);
+            taskDiv.appendChild(editButton);
+            taskDiv.appendChild(deleteButton);
             taskContainer.appendChild(taskDiv);
+
             taskInput.value = "";
+            
+            console.log(taskList);
         }
     })
 })
